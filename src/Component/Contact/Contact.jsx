7a3a4e4d12c2544b73b8053fee05783c903bd0fc
toaha77 <istiaktoaha07@gmail.com>
2024-01-16 22:52:29 +0600

    contact section added

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,8 +1,21 @@
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_r2pfb7d', 'template_kjm4mo4', form.current, 'xf7oG-XpwmIFyNKiB')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div>
       <div
-        className="hero min-h-[70vh]"
+        className="hero min-h-[50vh]"
         style={{
           backgroundImage:
             "url(https://i.ibb.co/BjymyCH/oswald-elsaboath-ym-EI-DTS1g-unsplash.jpg)",
@@ -12,10 +25,55 @@ const Contact = () => {
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold salsa">Contact</h1>
-             
           </div>
         </div>
       </div>
+    <div>
+    <div className="mt-6 space-y-4">
+        <h2 className="text-blue-500">WRITE A LINE</h2>
+        <h2 className="salsa text-5xl">Get in Touch</h2>
+        <p>We at LONG BEACH HOTEL are always here to assist you. Whether you have a query about your upcoming stay, need more information about our services, or simply want to share your experiences, our dedicated team is ready to help. </p>
+      </div>
+      <div className='mt-6 space-y-4'>
+      <form ref={form} onSubmit={sendEmail}>
+         <div>
+         <label>Your Name</label>
+          <input
+            type="text"
+            className=" w-full input input-accent"
+            placeholder="Your Name"
+            name="user_name"
+            required
+          />
+         </div>
+          <label>Your Email</label>
+          <div>
+          <input
+            type="email"
+            className="  w-full input input-accent"
+            placeholder="Your Email"
+            name="user_email"
+            required
+          />
+          </div>
+        <div>
+        <label>Message</label> <br />
+          <textarea
+            className="w-full textarea input-accent p-1  rounded-sm   mt-2"
+            name="message"
+            id=""
+            cols="30"
+            placeholder="Your Message"
+            rows="2"
+            required
+          ></textarea>
+        </div>
+         <div className="flex items-center justify-center">
+         <input type="submit" className="btn bg-green-500 text-white hover:bg-green-500 text-white" value="Send" />
+         </div>
+        </form>
+      </div>
+    </div>
     </div>
   );
 };
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
             isPending
               ? "pending"
               : isActive
-              ? " font-semibold  underline"
+              ? "font-semibold  underline"
               : ""
           }
         >
@@ -69,7 +69,7 @@ const Navbar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn">Book Now</a>
   </div>
 </div>
         </div>
