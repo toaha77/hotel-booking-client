1150a31b78e8ec4819ec276664861c16d9f23dce
toaha77 <istiaktoaha07@gmail.com>
2024-01-21 19:37:28 +0600

    image upadated

--- src/pages/Rooms/RoomsCard.jsx
+++ src/pages/Rooms/RoomsCard.jsx
@@ -1,24 +1,29 @@
 import { useLoaderData } from "react-router-dom";
-
+import { CgProfile } from "react-icons/cg";
 const RoomsCard = () => {
   const loader = useLoaderData();
   return (
-    <div>
+    <div className="mt-8">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {loader.map((data) => (
           <div key={data._id}>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <div className="card rounded-none card-compact  bg-base-100 shadow-xl">
               <figure>
                 <img
                   src={data.img}
                   alt="rooms"
+                  className="lg:w-96 h-64"
                 />
               </figure>
               <div className="card-body">
                 <h2 className="card-title">{data.title}</h2>
+                <div className="flex items-center justify-center gap-1">
+                  <CgProfile className="text-2xl"></CgProfile>
+                  <p className="uppercase text-justify">{data.short_description}</p>
+                </div>
                 <p>{data.description}</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Buy Now</button>
+                <div className="card-actions justify-center">
+                  <button className="btn bg-blue-500 text-white hover:bg-blue-500 text-white">Book Now</button>
                 </div>
               </div>
             </div>
