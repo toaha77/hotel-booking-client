76b41f92263fb868b1b9848436e710caf80a0e0a
toaha77 <istiaktoaha07@gmail.com>
2024-01-22 19:09:16 +0600

    price added

--- src/pages/Package/Package.jsx
+++ src/pages/Package/Package.jsx
@@ -7,9 +7,9 @@ const Package = () => {
           backgroundImage: "url(https://i.ibb.co/7KNqh2H/h.jpg)",
         }}
       >
-        <div className="flex items-center justify-center px-10 py-16">
+        <div className="flex flex-col lg:flex-row items-center justify-center px-10 py-16">
           <div className="w-1/2"></div>
-          <div className="w-1/2 bg-[#f5d1d1] p-8">
+          <div className="lg:w-1/2 bg-[#f5d1d1] p-8">
             <div className="text-center">
             <p className=" text-3xl uppercase">Enjoy</p>
             <p className="text-9xl  font-semibold text-rose-400">
--- src/pages/Rooms/RoomsCard.jsx
+++ src/pages/Rooms/RoomsCard.jsx
@@ -17,13 +17,18 @@ const RoomsCard = () => {
               </figure>
               <div className="card-body">
                 <h2 className="card-title">{data.title}</h2>
-                <div className="flex items-center justify-center gap-1">
+              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-center gap-1">
                   <CgProfile className="text-2xl"></CgProfile>
                   <p className="uppercase text-justify">{data.short_description}</p>
                 </div>
+                <div>
+                  <p className="text-xl">Staring From : <span className="text-blue-500">{data.price} / Night</span></p>
+                </div>
+              </div>
                 <p>{data.description}</p>
                 <div className="card-actions justify-center">
-                  <button className="btn bg-blue-500 text-white hover:bg-blue-500 text-white">Book Now</button>
+                  <button className="btn  btn-outline  hover:bg-blue-500 text-white">Book Now</button>
                 </div>
               </div>
             </div>
